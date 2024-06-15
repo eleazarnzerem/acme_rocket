e8a9f386ad27f5b7e60c21441a20294790a08a9a
Eleazar Nzerem <eleazarnzereme20201@gmail.com>
2024-06-16 00:07:41 +0100

    progress3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./build/**.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)'},
+      }
+    },
   },
   plugins: [],
 }
